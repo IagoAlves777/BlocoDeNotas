5cd19cfa239c45494fae14b29969636925f44e9d
Iago da Silva Alves <Iagosilva@camed.net.br>
2021-06-04 12:17:38 -0300

    Cards criados dinamicamente

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
+  constructor(props){
+    super(props);
+    this.title = "";
+    this.texto = "";
+  }
+  _handleMudancaTitulo(event){
+    event.stopPropagation();
+    this.title = event.target.value;
+  }
+  _handleMudancaTexto(event){
+    event.stopPropagation();
+    this.texto = event.target.value;
+  }
+  _criarNota(event){
+    event.stopPropagation();
+    event.preventDefault();
+    this.props.criarNota(this.title, this.texto);
+  }
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro "
+        onSubmit ={this._criarNota.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this._handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
