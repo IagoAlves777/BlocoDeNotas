5fdc19ad92a809173ca34bfa619cf7488e750c98
IagoAlves777 <iagox.alves@gmail.com>
2021-06-05 19:50:07 -0300

    ao enviar a nota os campos do form são limpos

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -10,11 +10,11 @@ class FormularioCadastro extends Component {
   }
   _handleMudancaTitulo(event){
     event.stopPropagation();
-    this.state.title = event.target.value;
+    this.setState({title: event.target.value});
   }
   _handleMudancaTexto(event){
     event.stopPropagation();
-    this.state.texto = event.target.value;
+    this.setState({texto: event.target.value});
   }
   _criarNota(event){
     if(this.state.title.length == 0){
@@ -38,12 +38,15 @@ class FormularioCadastro extends Component {
           placeholder="Título"
           className="form-cadastro_input"
           onChange={this._handleMudancaTitulo.bind(this)}
+          value={this.state.title}
+          
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
           onChange={this._handleMudancaTexto.bind(this)}
+          value={this.state.texto}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
